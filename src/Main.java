f4c378ad4e663237be36bccc9790f296e570fa08
Alen Mujkanović <mujkalen@gmail.com>
2024-10-07 21:57:32 +0200

    homework

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alen Mujkanovic!!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter first integer");
+        int numOne = sc.nextInt();
+
+        System.out.println("Please enter second integer");
+        int numTwo = sc.nextInt();
+
+        int result = numOne * numTwo;
+
+        System.out.println(numOne + " * " + numTwo + " = " + result);
     }
-
-   
-
 }
