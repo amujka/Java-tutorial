104e833b6ecc4294a189370d5edc8a1f0277f77d
Alen Mujkanović <mujkalen@gmail.com>
2024-10-09 19:17:06 +0200

    for loop

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       int evenCount = 0;
+       int oddCount = 0;
 
-        System.out.println("Please enter first integer");
-        int numOne = sc.nextInt();
-
-        System.out.println("Please enter second integer");
-        int numTwo = sc.nextInt();
-
-        int result = numOne * numTwo;
-
-        System.out.println(numOne + " * " + numTwo + " = " + result);
+       for (int i = 1; i<= 100; i++){
+           if (i % 2 == 0){
+               evenCount++;
+           }else {
+               oddCount++;
+           }
+       }
+       System.out.printf("Between(including) 1 and 100 there are %d even and %d odd numbers",evenCount, oddCount);
     }
 }
