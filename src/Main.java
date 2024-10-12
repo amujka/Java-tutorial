4e9fb4edab16f68acf49b2d68ca20dcc58d4753a
Alen Mujkanović <mujkalen@gmail.com>
2024-10-12 11:44:27 +0200

    revert changes to default

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int num;
 
-        System.out.println("Please enter number:");
-        num = scanner.nextInt();
-        while (num != 0){
-            sum+=num;
-            System.out.println("Please enter number:");
-            num = scanner.nextInt();
-        }
-        System.out.println("sum is :"+ sum);
     }
 }
