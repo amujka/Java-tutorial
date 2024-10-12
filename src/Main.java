340df11d3370b78563ec76a9aaff60e0ced9fb60
Alen Mujkanović <mujkalen@gmail.com>
2024-10-12 12:23:58 +0200

    zadatak_3

--- src/Main.java
+++ src/Main.java
@@ -25,16 +25,38 @@ public class Main {
 
         //Zadatak 2: Brojanje znamenki broja
 
-        System.out.println("Please enter integer");
-        int num = scanner.nextInt();
-        int tempNum = num;
-        int digitCount = 0;
+//        System.out.println("Please enter integer");
+//        int num = scanner.nextInt();
+//        int tempNum = num;
+//        int digitCount = 0;
+//
+//        while (num > 0){
+//            num = num /10;
+//            digitCount++;
+//        }
+//
+//        System.out.println(tempNum + " has " + digitCount + " digits");
 
-        while (num > 0){
-            num = num /10;
-            digitCount++;
-        }
+        //Zadatak 3: Unos brojeva dok ne postignemo određeni zbroj
 
-        System.out.println(tempNum + " has " + digitCount + " digits");
+        int goalNum = 10;
+        int sum = 0;
+        int count = 0;
+        int num;
+
+        do {
+            System.out.println("Please enter integer");
+            num = scanner.nextInt();
+            if (num < 0 ){
+                System.out.println("Number must be positive");
+            } else if (num == 0){
+                break;
+            } else{
+                sum += num;
+            }
+            count ++;
+        }while (sum < goalNum);
+
+        System.out.println(count + " tries, sum is " + sum);
     }
 }
