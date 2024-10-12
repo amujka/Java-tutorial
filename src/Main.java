bd3ca23a9f708223c5bb742cfebde98e47800508
Alen Mujkanović <mujkalen@gmail.com>
2024-10-12 12:00:28 +0200

    zadatak_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        //Zadatak 1: Brojevi od 1 do N
+
+        int nNumber;
+        int sum = 0;
+
+        do {
+            System.out.println("Please enter integer");
+            nNumber = scanner.nextInt();
+            if (nNumber < 0) {
+                System.out.println("Number must be positive integer");
+            }
+        } while (nNumber < 0);
+
+        for (int i = 0; i <= nNumber; i++) {
+            sum+=i;
+        }
+
+        System.out.println("Sum of all numbers up to " + nNumber + " is " + sum);
     }
 }
