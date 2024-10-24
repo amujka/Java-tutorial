c3ce5f32218e7f1ba8887ce0b1cf9a9ac5d209da
Alen Mujkanović <mujkalen@gmail.com>
2024-10-24 20:22:14 +0200

    1.zadatak

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numberList = new ArrayList<>();
+        int number;
 
+        do {
+            System.out.println("Enter number or 0 to exit" );
+            number = scanner.nextInt();
+            if (number != 0){
+                numberList.add(number);
+            }
+        } while (number != 0);
+
+        boolean isSorted = checkOrder(numberList);
+        if (isSorted){
+            System.out.println("List is sorted");
+        }else {
+            System.out.println("List is not sorted");
+        }
+    }
+
+    static boolean checkOrder(ArrayList<Integer> list){
+        int lastIndex = list.indexOf(list.size()-1);
+        for (int i = 0; i < list.size(); i++) {
+            if (i < lastIndex && list.get(i) > list.get(i+1)){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
