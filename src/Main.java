71a7d1aaecd3f5dfa6edde4bc1593ade249ed766
Alen Mujkanović <mujkalen@gmail.com>
2024-10-25 11:14:15 +0200

    1. zadatak

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+       //1. zadatak
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please write something");
+        String [] wordArray =  scanner.nextLine().split(" ");
+
+        findLongerWords(wordArray);
+        findLongestWord(wordArray);
+        stringAverageLength(wordArray);
     }
 
+    static void findLongerWords(String[] wordArray){
+        String longerWords = "";
+        for (String word : wordArray) {
+            if (word.length() > 5){
+                longerWords = longerWords.concat(word + " ");
+            }
+        }
+        if (longerWords.isEmpty()){
+            System.out.println("No words longer than 5");
+        }else {
+            System.out.println("Words longer than 5 letters: " + longerWords.toUpperCase());
+        }
+    }
+
+    static void findLongestWord(String[] wordArray){
+        String longestWord = "";
+        for (String word : wordArray) {
+            if (word.length() > longestWord.length()){
+               longestWord = word;
+            }
+        }
+        System.out.println("First longest word: " + longestWord.toUpperCase());
+    }
+
+    static void stringAverageLength(String[] wordArray){
+        int lettersSum = 0;
+        for (String word : wordArray){
+            lettersSum += word.length();
+        }
+        float wordAvg = (float)lettersSum / wordArray.length;
+        System.out.println("Word average length is: " + wordAvg);
+    }
 }
