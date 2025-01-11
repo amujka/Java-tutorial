f0764f9ea69ff969918d23de09b7949bdd00ce8a
Alen Mujkanović <mujkalen@gmail.com>
2025-01-11 15:33:36 +0100

    1. zadatak

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        wordCounter();
     }
 
+    static void wordCounter() {
+        Map<String, Integer> words = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the text:");
+        String text = scanner.nextLine();
+        String[] textArray = text.split("[\\s\\p{Punct}]+");
+
+        for (String word : textArray) {
+            word = word.toLowerCase();
+            if (words.containsKey(word)) {
+                int count = words.get(word) + 1;
+                words.put(word, count);
+            } else {
+                words.put(word, 1);
+            }
+        }
+
+        words.forEach((word, count) -> System.out.println(word + " -> " + count));
+    }
 }
