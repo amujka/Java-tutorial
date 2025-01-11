ec485eec68f56a6ccef42f3ab558e3965bd0d702
Alen Mujkanović <mujkalen@gmail.com>
2025-01-11 18:21:19 +0100

    2.zadatak

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        wordCounter();
+        //wordCounter();
+        printUniqueNumbers();
+        /*
+        Zadatak 2: Popis jedinstvenih vrijednosti koristeći Set
+        Napišite program koji:
+        Od korisnika traži unos brojeva (razdvojenih razmakom).
+        Koristi HashSet za izdvajanje samo jedinstvenih brojeva (bez ponavljanja).
+        Ispisuje sve jedinstvene brojeve.
+        Traži najveći i najmanji broj koristeći odgovarajuće metode skupa.
+        */
     }
 
     static void wordCounter() {
@@ -27,5 +34,32 @@ public class Main {
         }
 
         words.forEach((word, count) -> System.out.println(word + " -> " + count));
+    }
+
+    static void printUniqueNumbers() {
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the numbers:");
+        String text = scanner.nextLine();
+        String[] numbersArray = text.split(" ");
+        for (String num : numbersArray) {
+            uniqueNumbers.add(Integer.parseInt(num));
+        }
+
+        System.out.println(uniqueNumbers);
+
+        int minNum = Integer.MAX_VALUE;
+        int maxNum = Integer.MIN_VALUE;
+        for (int number : uniqueNumbers) {
+            if (number < minNum) {
+                minNum = number;
+            }
+            if (number > maxNum) {
+                maxNum = number;
+            }
+        }
+
+        System.out.println("Smallest number: " + Collections.min(uniqueNumbers));
+        System.out.println("Biggest number: " + Collections.max(uniqueNumbers));
     }
 }
