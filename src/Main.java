7b5aa2d980866d9f71ab45e948c0475490ef37e4
Alen Mujkanović <mujkalen@gmail.com>
2025-01-13 15:54:39 +0100

    3.zadatak

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,86 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //wordCounter();
-        printUniqueNumbers();
+        HashMap<String, List<String>> articlesByCategory = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Select 1.Add category 2.Add article to category 3.Print category 4.Print All 0. Exit");
+            String option = scanner.nextLine();
+            switch (option) {
+                case "1":
+                    addCategory(scanner, articlesByCategory);
+                    break;
+                case "2":
+                    addArticlesByCategory(scanner, articlesByCategory);
+                    break;
+                case "3":
+                    printOneCategoryContent(scanner, articlesByCategory);
+                    break;
+                case "4":
+                    printAll(articlesByCategory);
+                    break;
+                case "0":
+                    System.out.println("Exiting the program.");
+                    return;
+            }
+
+        }
+
         /*
-        Zadatak 2: Popis jedinstvenih vrijednosti koristeći Set
-        Napišite program koji:
-        Od korisnika traži unos brojeva (razdvojenih razmakom).
-        Koristi HashSet za izdvajanje samo jedinstvenih brojeva (bez ponavljanja).
-        Ispisuje sve jedinstvene brojeve.
-        Traži najveći i najmanji broj koristeći odgovarajuće metode skupa.
+        Napišite program koji simulira sustav za upravljanje proizvodima:
+        Koristi HashMap gdje je ključ ime kategorije (npr. "Voće", "Povrće"), a vrijednost je Lista proizvoda u toj kategoriji.
+        Dodaje proizvode u kategorije (npr. "Jabuka" u "Voće").
+        Omogućuje prikaz svih proizvoda unutar određene kategorije.
+        Prikazuje sve kategorije i proizvode u njima.
         */
+
     }
 
-    static void wordCounter() {
-        Map<String, Integer> words = new HashMap<>();
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the text:");
-        String text = scanner.nextLine();
-        String[] textArray = text.split("[\\s\\p{Punct}]+");
+    static void printAll(HashMap<String, List<String>> articlesByCategory) {
+        articlesByCategory.forEach((category, articlesList) -> {
+            System.out.println(category + ":" + articlesList);
+        });
+    }
 
-        for (String word : textArray) {
-            word = word.toLowerCase();
-            if (words.containsKey(word)) {
-                int count = words.get(word) + 1;
-                words.put(word, count);
+    static void printOneCategoryContent(Scanner scanner, HashMap<String, List<String>> articlesByCategory) {
+        System.out.println("Enter category name");
+        String category = scanner.nextLine();
+        List<String> list = articlesByCategory.get(category);
+        System.out.println("All articles in category " + category);
+        System.out.println(list);
+    }
+
+    static void addCategory(Scanner scanner, HashMap<String, List<String>> articlesByCategory) {
+        String category;
+        while (true) {
+            System.out.println("Add new category or enter 0 to exit");
+            category = scanner.nextLine();
+            if (category.equals("0")) break;
+            if (!articlesByCategory.containsKey(category)) {
+                articlesByCategory.put(category, new ArrayList<>());
             } else {
-                words.put(word, 1);
+                System.out.println("Category already exists");
             }
         }
-
-        words.forEach((word, count) -> System.out.println(word + " -> " + count));
     }
 
-    static void printUniqueNumbers() {
-        HashSet<Integer> uniqueNumbers = new HashSet<>();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the numbers:");
-        String text = scanner.nextLine();
-        String[] numbersArray = text.split(" ");
-        for (String num : numbersArray) {
-            uniqueNumbers.add(Integer.parseInt(num));
+    static void addArticlesByCategory(Scanner scanner, HashMap<String, List<String>> articlesByCategory) {
+        System.out.println("Enter category:");
+        String selectedCategory = scanner.nextLine();
+        List<String> articleList = articlesByCategory.get(selectedCategory);
+
+        while (true) {
+            System.out.println("Add article to list " + selectedCategory + " or enter 0 to exit");
+            String article = scanner.nextLine();
+            if (article.equals("0")) break;
+            articleList.add(article);
         }
-
-        System.out.println(uniqueNumbers);
-
-        int minNum = Integer.MAX_VALUE;
-        int maxNum = Integer.MIN_VALUE;
-        for (int number : uniqueNumbers) {
-            if (number < minNum) {
-                minNum = number;
-            }
-            if (number > maxNum) {
-                maxNum = number;
-            }
-        }
-
-        System.out.println("Smallest number: " + Collections.min(uniqueNumbers));
-        System.out.println("Biggest number: " + Collections.max(uniqueNumbers));
     }
 }
